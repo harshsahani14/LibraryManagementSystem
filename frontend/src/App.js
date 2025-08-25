a2f77969fffac85cc4138f2ba90768bee25f8093
Harsh10 <harshsahani078@gmail.com>
2025-08-25 22:13:58 +0530

    Some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,59 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import DashBoardPage from './pages/DashBoardPage'
-import AddBookPage from './pages/AddBookPage'
-import SearchBookPage from './pages/SearchBookPage'
-import ViewBookPage from './pages/ViewBook'
-import BorrowBookPage from './pages/BorrowBookPage'
-import ViewReportPage from './pages/ViewReportPage'
-import NotFoundPage from './pages/NotFoundPage'
-import { Toaster } from 'react-hot-toast'
-import {useSelector} from 'react-redux'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashBoardPage from "./pages/DashBoardPage";
+import AddBookPage from "./pages/AddBookPage";
+import SearchBookPage from "./pages/SearchBookPage";
+import ViewBookPage from "./pages/ViewBook";
+import BorrowBookPage from "./pages/BorrowBookPage";
+import ViewReportPage from "./pages/ViewReportPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const App = () => {
-
-  const userId = useSelector((state) => state.userId.value)
+  const userId = useSelector((state) => state.userId.value);
 
   return (
     <div className="">
-      
       <Routes>
-
-        {
-
-            !userId && 
-            <>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-            </>
-        }
-        {
-
-          userId && (
-            <>
-              <Route path="/dashboard" element={<DashBoardPage />} />
-              <Route path="/addBook" element={<AddBookPage />} />
-              <Route path="/searchBook" element={<SearchBookPage />} />
-              <Route path="/viewBook" element={<ViewBookPage />} />
-              <Route path="/borrowBook" element={<BorrowBookPage />} />
-              <Route path="/viewReport" element={<ViewReportPage />} />
-            </>
-          )
-
-        }
-
-        <Route path='/*' element={<NotFoundPage />} />
+        <Route
+          path="/"
+          element={userId > 0 ? <Navigate to="/dashboard" /> : <LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={userId > 0 ? <Navigate to="/dashboard" /> : <RegisterPage />}
+        />
+        <Route
+          path="/dashboard"
+          element={userId === 0 ? <Navigate to="/" /> : <DashBoardPage />}
+        />
+        <Route
+          path="/addBook"
+          element={userId === 0 ? <Navigate to="/" /> : <AddBookPage />}
+        />
+        <Route
+          path="/searchBook"
+          element={userId === 0 ? <Navigate to="/" /> : <SearchBookPage />}
+        />
+        <Route
+          path="/viewBook"
+          element={userId === 0 ? <Navigate to="/" /> : <ViewBookPage />}
+        />
+        <Route
+          path="/borrowBook"
+          element={userId === 0 ? <Navigate to="/" /> : <BorrowBookPage />}
+        />
+        <Route
+          path="/viewReport"
+          element={userId === 0 ? <Navigate to="/" /> : <ViewReportPage />}
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
